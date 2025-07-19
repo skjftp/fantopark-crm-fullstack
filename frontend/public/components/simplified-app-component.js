111b0ffc4e055cbebd37a038a8bb8a942b656560
skjftp <sumit.jha@fantopark.com>
2025-07-19 11:50:00 +0530

    Update simplified-app-component.js

--- frontend/public/components/simplified-app-component.js
+++ frontend/public/components/simplified-app-component.js
@@ -3514,23 +3514,6 @@ window.toggleMobileMenu = function() {
     }
 };
 
-// Add resize effect
-React.useEffect(() => {
-    const handleResize = () => {
-        const isMobile = window.innerWidth <= 768;
-        if (isMobile !== isMobileView) {
-            setIsMobileView(isMobile);
-            window.isMobileView = isMobile;
-        }
-    };
-
-    window.addEventListener('resize', handleResize);
-    
-    return () => {
-        window.removeEventListener('resize', handleResize);
-    };
-}, [isMobileView]);
-
 // Additional function to ensure horizontal scroll is prevented
 window.preventHorizontalScroll = function() {
     // This function runs periodically to catch any dynamic content
